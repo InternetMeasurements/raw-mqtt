05b27ae4f9e35424612b46739a1e39f224257135
Federico Cristofani <federico.cristofani98@outlook.it>
2024-02-09 15:46:18 +0100

    Stream client refactoring to remove unnecessary overhead

--- raw-mqtt-lib/src/client/stream_client.rs
+++ raw-mqtt-lib/src/client/stream_client.rs
@@ -5,29 +5,29 @@ use mqttbytes::v4::Publish;
 use mqttbytes::QoS;
 use bytes::{BytesMut};
 use log::debug;
-use tokio::sync::mpsc::Sender;
 use tokio::task::yield_now;
+use tokio_util::sync::CancellationToken;
 
 use crate::network::channel_network::ChannelNetwork;
 use crate::network::transport::Transport;
-use crate::{ACK_PACKET_SIZE, MqttMessage, parse_packet, Version};
+use crate::{ACK_PACKET_SIZE, parse_packet, Version};
 use crate::client::client::Client;
 use crate::network::network::Network;
 
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct StreamMqttClient {
     _client: Client<ChannelNetwork>,
-    pending_acks: Arc<AtomicU16>,
-    tx: Option<Sender<MqttMessage>>
+    pending_requests: Arc<AtomicU16>,
+    cancellation_tkn: CancellationToken
 }
 
 impl Default for StreamMqttClient {
     fn default() -> StreamMqttClient {
         StreamMqttClient{
             _client: Client::default(),
-            pending_acks: Arc::new(AtomicU16::new(0)),
-            tx: None
+            pending_requests: Arc::new(AtomicU16::new(0)),
+            cancellation_tkn: CancellationToken::new()
         }
     }
 }
@@ -45,8 +45,8 @@ impl StreamMqttClient {
                 version,
                 insecure
             ),
-            pending_acks: Arc::new(AtomicU16::new(0)),
-            tx: None
+            pending_requests: Arc::new(AtomicU16::new(0)),
+            cancellation_tkn: CancellationToken::new()
         }
     }
 
@@ -56,52 +56,27 @@ impl StreamMqttClient {
     pub async fn connect(&mut self) -> Result<(), Box<dyn Error>> {
         self._client.connect().await?;
 
-        let (tx, mut rx) = tokio::sync::mpsc::channel::<MqttMessage>(1024);
-        self.tx = Some(tx);
-        let network = self._client.network.clone();
+        let pending_requests = self.pending_requests.clone();
+        let mut recv_network = self._client.network.clone();
         let version = self._client.version.clone();
-        let pkid = self._client.pkid.clone();
-        let pending_acks = self.pending_acks.clone();
+        let cancellation_tkn = self.cancellation_tkn.clone();
 
-
-        let _ = tokio::spawn(async move {
-
-            loop {
-                let v = version.clone();
-                let message = rx.recv().await.unwrap();
-
-                let mut pub_req = Publish::new(message.topic, message.qos, message.payload);
-                let mut send_buffer = BytesMut::new();
-                let pending_acks_tx = pending_acks.clone();
-                let pending_acks_rx = pending_acks.clone();
-
-                pub_req.pkid = pkid.clone().fetch_add(1, Ordering::SeqCst);
-                if pub_req.pkid == 0 {
-                    pub_req.pkid = pkid.clone().fetch_add(1, Ordering::SeqCst);
-                }
-                pub_req.write(&mut send_buffer).expect("Packet serialization failed");
-
-                let mut send_network = network.clone();
-                let mut recv_network = network.clone();
-
-                tokio::spawn(async move {
-                    send_network.send(send_buffer.as_ref()).await.unwrap();
-                    if message.qos == QoS::AtMostOnce {
-                        pending_acks_tx.clone().fetch_sub(1, Ordering::SeqCst);
-                    }
-
-                });
-
-                if message.qos == QoS::AtLeastOnce {
-                    tokio::spawn(async move {
-                        let mut recv_buffer = recv_network.recv(ACK_PACKET_SIZE).await.unwrap();
-                        let packet = parse_packet(&mut recv_buffer, 1024, &v).unwrap();
-                        debug!("Received ack: {:?}", packet);
-                        pending_acks_rx.clone().fetch_sub(1, Ordering::SeqCst);
-                    });
+        // Spawn receiver task (for acks)
+        tokio::spawn(async move {
+            'rx_loop: loop {
+                tokio::select! {
+                _ = cancellation_tkn.cancelled() => {
+                    break 'rx_loop;
+                },
+                recv_buffer = recv_network.recv(ACK_PACKET_SIZE) => {
+                    let packet = parse_packet(&mut recv_buffer.unwrap(), 1024, &version).unwrap();
+                    debug!("Received ack: {:?}", packet);
+                    pending_requests.fetch_sub(1, Ordering::SeqCst);
                 }
             }
+            }
         });
+
         Ok(())
     }
 
@@ -116,11 +91,13 @@ impl StreamMqttClient {
      * Disconnect from broker and stop network tasks. The client cannot be reused after this call.
      */
     pub async fn disconnect(&mut self) -> Result<(), Box<dyn Error>> {
-        while self.pending_acks.fetch_add(0, Ordering::SeqCst) > 0 {
+
+        while self.pending_requests.fetch_add(0, Ordering::SeqCst) > 0 {
             yield_now().await;
         }
-        debug!("{:?}", self.pending_acks);
+        debug!("{:?}", self.pending_requests);
         self._client.disconnect().await?;
+        self.cancellation_tkn.cancel();
         self._client.network.close().await
     }
 
@@ -128,15 +105,41 @@ impl StreamMqttClient {
      * Publish a stream of messages to a topic.
      */
     pub async fn stream_publish(&self, topic: String, payload: String, qos: QoS) -> Result<(), Box<dyn Error>> {
-        self.tx.as_ref().unwrap().send(MqttMessage{
-            topic,
-            payload,
-            qos
-        }).await?;
+        let mut network = self._client.network.clone();
+        let mut pub_req = Publish::new(topic, qos, payload);
 
-        self.pending_acks.fetch_add(1, Ordering::SeqCst);
+        // Set packet id (if needed)
+        if qos == QoS::AtLeastOnce || qos == QoS::ExactlyOnce {
+            pub_req.pkid = self._client.pkid.fetch_add(1, Ordering::SeqCst);
+            if pub_req.pkid == 0 {
+                pub_req.pkid = self._client.pkid.fetch_add(1, Ordering::SeqCst);
+            }
+        }
+
+        // Serialize packet
+        let mut send_buffer = BytesMut::new();
+        pub_req.write(&mut send_buffer).expect("Packet serialization failed");
+
+        #[cfg(not(feature="constrained-rate"))] {
+            // Increment pending requests
+            self.pending_requests.fetch_add(1, Ordering::SeqCst);
+            let pending_requests = self.pending_requests.clone();
+            // Spawn sender task
+            tokio::spawn(async move {
+                // Clone data for async tasks
+                network.send(send_buffer.as_ref()).await.unwrap();
+                if qos == QoS::AtMostOnce {
+                    pending_requests.fetch_sub(1, Ordering::SeqCst);
+                }
+            });
+        }
+        #[cfg(feature="constrained-rate")]{
+            network.send(send_buffer.as_ref()).await.unwrap();
+            if qos != QoS::AtMostOnce {
+                self.pending_requests.fetch_add(1, Ordering::SeqCst);
+            }
+        }
 
         Ok(())
     }
-
 }
